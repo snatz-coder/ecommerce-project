4a7af778a8555c6f6bce481f7cc8a21320e9dc5f
Sujane Natasha Lopez <snazlopez@gmail.com>
2026-02-15 18:02:28 +0000

    Create itegration tests

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -18,13 +18,13 @@ export function Product({ product, loadCart }) {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" data-testid="product-image" src={product.image} />
       </div>
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
 
       <div className="product-rating-container">
         <img
-          className="product-rating-stars"
+          className="product-rating-stars" data-testid="product-rating"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
         <div className="product-rating-count link-primary">
